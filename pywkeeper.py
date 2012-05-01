22310f2b2c731b12f2c26a0b44df1c857d4137e6
Ali Kaafarani <ali@kvikshaug.no>
2012-05-01 18:05:50 +0200

    Put stuff in 'data' folder

--- pywkeeper.py
+++ pywkeeper.py
@@ -11,9 +11,9 @@ BLOCK_LENGTH = 16
 DEFAULT_PASSWORD_LENGTH = 25
 KEY_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-KEY_FILE = os.path.dirname(__file__) + '/key'
-ENCRYPTED_FILE = os.path.dirname(__file__) + '/data'
-DECRYPTED_FILE = os.path.dirname(__file__) + '/tmp'
+KEY_FILE = os.path.dirname(__file__) + '/data/key'
+ENCRYPTED_FILE = os.path.dirname(__file__) + '/data/data'
+DECRYPTED_FILE = os.path.dirname(__file__) + '/data/tmp'
 
 EOT_CHAR = b'\x04'
 
