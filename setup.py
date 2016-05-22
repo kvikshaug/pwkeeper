9defd3bacdf1e22a7c4d54bf35ce1055a932d9a6
Ali Kaafarani <ali@kvikshaug.no>
2016-05-22 13:37:35 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+name = 'pwkeeper'
+VERSION = '1.0.0'
+
+setup(
+    name=name,
+    packages=[name],
+    version=VERSION,
+    description='Simple terminal-based password manager',
+    long_description='See https://github.com/kvikshaug/pwkeeper/blob/master/README.md',
+    author='Ali Kaafarani',
+    author_email='ali@kvikshaug.no',
+    url='https://github.com/kvikshaug/pwkeeper/',
+    download_url='https://github.com/kvikshaug/pwkeeper/tarball/v%s' % (VERSION),
+    keywords=['password', 'password-manager', 'terminal'],
+    license='WTFPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['pycrypto>=2.6.1,<3'],
+)
